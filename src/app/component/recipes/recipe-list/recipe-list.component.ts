bf910c309d37e32186f0c7c756d7ea46cb4a30c2
gehadkadrey <gehadkadrey@gmail.com>
2024-05-18 14:35:51 +0300

    edit im recipe list

--- src/app/component/recipes/recipe-list/recipe-list.component.ts
+++ src/app/component/recipes/recipe-list/recipe-list.component.ts
@@ -10,7 +10,7 @@ import { RecipeModel } from '../../../Models/recipe-model';
 export class RecipeListComponent {
 recipeList:RecipeModel[]=[  new RecipeModel("recipe1"," description of recipe1","/assets/images/recipe1.jpg"),
 new RecipeModel("recipe2","description of recipe2","/assets/images/recipe2.jpg"),
-new RecipeModel("recipe3","description of recipe3","/assets/images/recipe1.jpg")
+new RecipeModel("recipe3","description of recipe3","/assets/images/recipe3.jpg")
 ];
 @Output() selectOne=new EventEmitter<RecipeModel>();
 OnselectedElement(recipeDetails:RecipeModel)
